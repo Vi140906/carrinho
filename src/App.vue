5398a1111cd561c4518bbae5dc9fab97c4f9ef8a
Vi140906 <santosviih360@gmail.com>
2023-05-22 12:00:01 -0300

    ok

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+import { tenis } from '@/_data/tenis'
+
+</script>
+
+<template>
+
+  <h1> Santo's Skate </h1>
+  <div class="tenis">
+    <div class="card-tenis" v-for="tenis in tenis" :key="tenis.id">
+      <img :src="tenis.cover" class="tenis-capa" />
+      <p class="tenis-titulo">{{ tenis.title }}</p>
+      <p class="tenis-preco">{{ tenis.price }}</p>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+</style>
---
+++ src/_data/tenis.js
@@ -0,0 +1,39 @@
+export const tenis = [
+  {
+    id: 1,
+    title: "Tesla",
+    price: 349.9,
+    cover: "https://grindskateshop.com.br/wp-content/uploads/2023/05/tenis_tesla_coil_black_reflect_preto_branco_refletivo_1901_06_6684_2_0ca8498206ec08a05b6d210c21d0aff7-510x510.jpg",
+  },
+  // cover: link da imagem 
+  {
+    id: 2,
+    title: "Hocks",
+    price: 399.9,
+    cover: "https://grindskateshop.com.br/wp-content/uploads/2023/03/15_8_2_238_Tenis_Hocks_DLCII_Zezinho_Black-510x510.jpg"
+  },
+  {
+    id: 3,
+    title: "Vans",
+    price: 699.9,
+    cover: "https://grindskateshop.com.br/wp-content/uploads/2023/03/download-16-510x255.png"
+  },
+  {
+    id: 4,
+    title: "DC",
+    price: 599.9,
+    cover: "https://grindskateshop.com.br/wp-content/uploads/2022/10/1264539_tenis-new-flash-2-tx-dc-shoes-dc012abkw_z1_637522706024597760-510x510.jpg"
+  },
+  {
+    id: 5,
+    title: "Nike",
+    price: 799.9,
+    cover: "https://grindskateshop.com.br/wp-content/uploads/2022/08/tenis-masculino-nike-sb-chron-2-black-white-249440.jpg"
+  },
+  {
+    id: 6,
+    title: "Adidas",
+    price: 799.9,
+    cover: "https://grindskateshop.com.br/wp-content/uploads/2023/03/7ed0855435194229a525aad6009a0497_9366-510x510.png"
+  }
+]
